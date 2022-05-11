3de146239e3524d5e2c7559107b6fa9669f1a620
Mukaddam Khamraeva <khamraeva.m1124@gmail.com>
2022-05-11 16:47:30 +0500

    packages

--- app/src/main/java/com/talkon/talkon/activity/SignInActivity.kt
+++ app/src/main/java/com/talkon/talkon/activity/SignInActivity.kt
@@ -1,11 +1,12 @@
-package com.talkon.talkon
+package com.talkon.talkon.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.talkon.talkon.R
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_in)
     }
 }
---
+++ app/src/main/java/com/talkon/talkon/fragment/SearchFragment.kt
@@ -0,0 +1,4 @@
+package com.talkon.talkon.fragment
+
+class SearchFragment {
+}
