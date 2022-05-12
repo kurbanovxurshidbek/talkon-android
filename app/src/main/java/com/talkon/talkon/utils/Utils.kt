34b2cb8b5f87309c9e4d89e87255952574d5be26
SadullayevBotir <91020070+SadullayevBotir@users.noreply.github.com>
2022-05-12 09:13:31 +0500

    added Utils package

---
+++ app/src/main/java/com/talkon/talkon/utils/Utils.kt
@@ -0,0 +1,16 @@
+package com.talkon.talkon.utils
+
+import android.app.Application
+import android.app.Dialog
+import android.content.Context
+import android.content.Context.WINDOW_SERVICE
+import android.provider.Settings
+import android.util.DisplayMetrics
+import android.view.Window
+import android.view.WindowManager
+import android.widget.TextView
+import android.widget.Toast
+
+object Utils {
+
+}
