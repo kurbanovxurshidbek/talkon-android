674bc8354317ff3a45aa11c5fadc3e87a1572719
Javlonbek <javlonbeksobirov03@gmail.com>
2022-05-30 17:55:34 +0500

    nearly complete edit profile page

--- app/src/main/java/com/talkon/talkon/fragment/EditProfileFragment.kt
+++ app/src/main/java/com/talkon/talkon/fragment/EditProfileFragment.kt
@@ -1,24 +1,67 @@
 package com.talkon.talkon.fragment
 
+import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import com.sangcomz.fishbun.FishBun
+import com.sangcomz.fishbun.adapter.image.impl.GlideAdapter
 import com.talkon.talkon.R
+import kotlinx.android.synthetic.main.fragment_edit_profile.*
 
 class EditProfileFragment : BaseFragment() {
+
+    var pickedPhoto: Uri? = null
+    var allPhotos = ArrayList<Uri>()
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         initViews(view)
-        return view
     }
 
     private fun initViews(view: View?) {
 
+            iv_edit_profile_plus.setOnClickListener {
+                pickFishBunPhoto()
+            }
+    }
+
+    private val photoLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            allPhotos =
+                it.data?.getParcelableArrayListExtra(FishBun.INTENT_PATH) ?: arrayListOf()
+            pickedPhoto = allPhotos.get(0)
+            uploadUserPhoto()
+        }
+    }
+
+    private fun uploadUserPhoto() {
+        if (pickedPhoto == null) return
+//        StorageManager.uploadUserPhoto(pickedPhoto!!, object : StorageHandler {
+//            override fun onSuccess(imgUrl: String) {
+//                DatabaseManager.updateUserImage(imgUrl)
+                iv_edit_profile.setImageURI(pickedPhoto)
+            }
+
+
+    private fun pickFishBunPhoto() {
+        FishBun.with(this)
+            .setImageAdapter(GlideAdapter())
+            .setMaxCount(1)
+            .setMinCount(1)
+            .setSelectedImages(allPhotos)
+            .startAlbumWithActivityResultCallback(photoLauncher)
     }
 
 }
