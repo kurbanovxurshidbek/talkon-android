b0f2d1a6523f538c71192d590dbeda9cfeeb49e3
WINDOWS 10 <mirzayevshaxzod9@gmail.com>
2022-05-31 17:17:05 +0500

    balance

--- app/src/main/java/com/talkon/talkon/activity/BalanceActivity.kt
+++ app/src/main/java/com/talkon/talkon/activity/BalanceActivity.kt
@@ -2,6 +2,7 @@ package com.talkon.talkon.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.talkon.talkon.R
 
 /**
@@ -12,5 +13,8 @@ class BalanceActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_balance)
+
     }
+
+
 }
