55e246cdeb7a845684fa609ecba25b626c7daf9d
EshonkhujaKhaN <eshonxojaa6@gmail.com>
2022-06-02 17:56:55 +0500

    sdg

---
+++ app/src/main/java/com/talkon/talkon/fragment/CashFragment.kt
@@ -0,0 +1,28 @@
+package com.talkon.talkon.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.talkon.talkon.R
+
+
+class CashFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cash, container, false)
+    }
+}
---
+++ app/src/main/java/com/talkon/talkon/fragment/MyLessonsFragment.kt
@@ -0,0 +1,25 @@
+package com.talkon.talkon.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.talkon.talkon.R
+
+
+class MyLessonsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_lessons, container, false)
+    }
+}
