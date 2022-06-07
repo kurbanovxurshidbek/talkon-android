f87b5b78cedfd3ec5061c4123a80f4dd2bb075ed
EshonkhujaKhaN <eshonxojaa6@gmail.com>
2022-06-07 20:33:58 +0500

    fixed SearchStudentFragment.kt

--- app/src/main/java/com/talkon/talkon/fragment/SearchStudentFragment.kt
+++ app/src/main/java/com/talkon/talkon/fragment/SearchStudentFragment.kt
@@ -5,25 +5,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.airbnb.lottie.LottieAnimationView
+import com.skyfishjy.library.RippleBackground
 import com.talkon.talkon.R
 import com.talkon.talkon.fragment.BaseFragment
+import kotlinx.android.synthetic.main.fragment_search_student.*
 
 /**
  * In StudentFragment, student can talk with student, by random match
  */
 class SearchStudentFragment : BaseFragment() {
+
+    private var isClicked : Boolean = false
     lateinit var lottie_students: LottieAnimationView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_search_student, container, false)
-        initViews(view)
+        val view: View = inflater.inflate(R.layout.fragment_search_student, container, false)
         return view
     }
 
-    private fun initViews(view: View) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
 
+    private fun initViews() {
+        bt_search_partner.setOnClickListener {
+            ll_call.visibility = View.VISIBLE
+            ll_search_partner.visibility = View.GONE
+        }
+
+        tv_exit_search_student.setOnClickListener {
+            ll_call.visibility = View.GONE
+            ll_search_partner.visibility = View.VISIBLE
+        }
     }
 }
